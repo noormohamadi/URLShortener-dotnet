89bf9255e10e0ee1fa16c917d97d10a39f843ad9
mohamad <noormohamadi@aut.ac.ir>
2020-02-08 16:50:43 +0330

    Add test for redirect

--- tests/ApiTest.cs
+++ tests/ApiTest.cs
@@ -44,5 +44,54 @@ namespace tests
 
         }
 
+        [Fact]
+        public void GenerateTest3()
+        {
+            var body = new
+            {
+                Url = "bar/foo"
+            };
+            new RestAssured()
+              .Given()
+                .Header("Content-Type", "application/json")
+                .Body(body)
+              .When()
+                .Post("localhost:5000/urls")
+              .Then()
+                .TestStatus("test status", x => x == 400)
+                .Assert("test status");
+
+        }
+
+        [Fact]
+        public void RedirectTest1()
+        {
+            new RestAssured()
+              .Given()
+                .Header("Content-Type", "application/json")
+                .Header("Accept-Encoding", "utf-8")
+              .When()
+                .Get("localhost:5000/Redirect/12345678")
+              .Then()
+                .TestStatus("test status", x => x == 404)
+                .Assert("test status");
+
+        }
+
+        [Fact]
+        public void RedirectTest2()
+        {
+            new RestAssured()
+              .Given()
+                .Header("Content-Type", "application/json")
+                .Header("Accept-Encoding", "utf-8")
+              .When()
+                .Get("localhost:5000/Redirect/12345")
+              .Then()
+                .TestStatus("test status", x => x == 400)
+                .Assert("test status");
+
+        }
+
     }
 }
